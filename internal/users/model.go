64ee243ebf7b17cb62116031dd6ac7e0ed3a9cbf
hakrithy <hakrithykim2020@gmail.com>
2025-04-05 09:28:26 +0700

    add

--- internal/users/model.go
+++ internal/users/model.go
@@ -1,1 +1,11 @@
 package users
+
+import (
+	model "admin-phone-shop-api/pkg/model"
+)
+
+type ShowUserRequest struct {
+	PagingOption model.PagingOption `json:"paging_options" query:"paging_options"`
+	Sorts        []model.Sort       `json:"sorts,omitempty" query:"sorts"`
+	Filters      []model.Filter     `json:"filters,omitempty" query:"filters"`
+}
