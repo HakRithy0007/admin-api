c4c2d9420a8d10aed97a055756c5bd727259b20d
hakrithy <hakrithykim2020@gmail.com>
2025-04-07 09:38:22 +0700

    add

--- migrations/20250327093658_tbl_users.sql
+++ migrations/20250327093658_tbl_users.sql
@@ -7,11 +7,11 @@ CREATE TABLE tbl_users (
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     phone VARCHAR(15) DEFAULT NULL,
-     is_admin SMALLINT DEFAULT 0,
+    is_admin SMALLINT DEFAULT 0,
     login_session TEXT NULL,
     last_access TIMESTAMP WITHOUT TIME ZONE,
+    role_id INTEGER DEFAULT 0,
     profile_photo TEXT NULL,
-    phone_number VARCHAR(20) NULL,
     status_id SMALLINT DEFAULT 1,
     "order" INTEGER NULL DEFAULT 1,
     created_by INTEGER,
@@ -23,16 +23,16 @@ CREATE TABLE tbl_users (
 );
 
 -- +goose StatementBegin
-INSERT INTO tbl_users (user_name, password, email) VALUES
-    ('ADMIN1', '123456', 'admin1@example.com'),
-    ('ADMIN2', '123456', 'admin2@example.com'),
-    ('ADMIN3', '123456', 'admin3@example.com'),
-    ('ADMIN4', '123456', 'admin4@example.com'),
-    ('ADMIN5', '123456', 'admin5@example.com'),
-    ('ADMIN6', '123456', 'admin6@example.com'),
-    ('ADMIN7', '123456', 'admin7@example.com'),
-    ('ADMIN8', '123456', 'admin8@example.com'),
-    ('ADMIN9', '123456', 'admin9@example.com');
+INSERT INTO tbl_users (first_name, last_name, user_name, password, email) VALUES
+    ('Admin', 'One', 'ADMIN1', '123456', 'admin1@example.com'),
+    ('Admin', 'Two', 'ADMIN2', '123456', 'admin2@example.com'),
+    ('Admin', 'Three', 'ADMIN3', '123456', 'admin3@example.com'),
+    ('Admin', 'Four', 'ADMIN4', '123456', 'admin4@example.com'),
+    ('Admin', 'Five', 'ADMIN5', '123456', 'admin5@example.com'),
+    ('Admin', 'Six', 'ADMIN6', '123456', 'admin6@example.com'),
+    ('Admin', 'Seven', 'ADMIN7', '123456', 'admin7@example.com'),
+    ('Admin', 'Eight', 'ADMIN8', '123456', 'admin8@example.com'),
+    ('Admin', 'Nine', 'ADMIN9', '123456', 'admin9@example.com');
 -- +goose StatementEnd
 
 -- +goose Down
