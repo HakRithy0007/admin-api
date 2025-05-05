926f139e0822da73e008573ff008764132ea81c7
hakrithy <hakrithykim2020@gmail.com>
2025-05-05 10:15:23 +0700

    add code on the module user

--- internal/users/repository.go
+++ internal/users/repository.go
@@ -4,6 +4,7 @@ import (
 	"admin-phone-shop-api/pkg/custom_log"
 	custom_models "admin-phone-shop-api/pkg/model"
 	error_response "admin-phone-shop-api/pkg/utils/error"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
@@ -25,13 +26,30 @@ func NewUserRepoImpl(uCtx *custom_models.AdminContext, db_pool *sqlx.DB) *UserRe
 }
 
 func (u UserRepoImpl) CreateUser(createUserReq CreateUserRequest) (*UserResponese, *error_response.ErrorResponse) {
+	msg := error_response.ErrorResponse{}
 
-	// Transaction 
+	// Transaction
 	tx, err := u.db_pool.Beginx()
 	if err != nil {
 		custom_log.NewCustomLog("transaction_start_failed", err.Error(), "error")
-		return nil, &error_response.ErrorResponse{
-			MessageID: ,
-		}
+		return nil, msg.NewErrorResponse("transaction_start_failed", fmt.Errorf("transaction start failed"))
 	}
+
+	// Commit transaction
+	err = tx.Commit()
+	if err != nil {
+		tx.Rollback()
+		custom_log.NewCustomLog("transaction_end_failed", err.Error(), "error")
+		return nil, msg.NewErrorResponse("transaction_end_failed", fmt.Errorf("could not commit transaction"))
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			tx.Rollback()
+			custom_log.NewCustomLog("get_current_game_panic", fmt.Sprintf("%v", p), "error")
+			panic(p)
+		} else {
+			tx.Rollback()
+		}
+	}()
 }
